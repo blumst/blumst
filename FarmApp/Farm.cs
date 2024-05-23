e89de40b64923b3a7a654aac611c3e932ca6615a
Mariia Khomych <masha.homich.696@gmail.com>
2024-05-23 20:02:36 +0300

    Сreated the "Farm" ConsoleApp

---
+++ FarmApp/Farm.cs
@@ -0,0 +1,89 @@
+﻿namespace FarmApp
+{
+    public class Farm
+    {
+        private int _fruitBoxes;
+        private int _veggieBoxes;
+
+        public Farm()
+        {
+            _fruitBoxes = 0;
+            _veggieBoxes = 0;
+        }
+
+        public void AddBox(string boxType, int quantity)
+        {
+            try
+            {
+                switch(boxType.ToLower().Trim())
+                {
+                    case "fruit":
+                        _fruitBoxes += quantity;
+                        break;
+                    case "vegetable":
+                        _veggieBoxes += quantity;
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown type of box - {boxType}.");
+                }
+
+                PrintInfo("add", quantity, boxType);
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured while adding boxes: {ex.Message}");
+            }
+        }
+
+        public void RemoveBox(string boxType, int quantity)
+        {
+            try
+            {
+                switch (boxType.ToLower().Trim())
+                {
+                    case "fruit":
+                        if (_fruitBoxes >= quantity)
+                            _fruitBoxes -= quantity;
+                        else
+                            throw new InvalidOperationException($"Not enough {boxType} boxes to remove.");
+                        break;
+                    case "vegetable":
+                        if (_veggieBoxes >= quantity)
+                            _veggieBoxes -= quantity;
+                        else
+                            throw new InvalidOperationException($"Not enough {boxType} boxes to remove.");
+                        break;
+                    default:
+                        throw new ArgumentException("Unknown type of box.");
+                }
+
+                PrintInfo("remove", quantity, boxType);
+
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured while removing boxes: {ex.Message}");
+            }
+
+        }
+
+        public void PrintInfo(string operationType, int quantity, string boxType)
+        {
+            if (operationType.ToLower().Trim() == "add")
+                Console.WriteLine($"{quantity} boxes of {boxType} added to the farm.");
+            else if (operationType.ToLower().Trim() == "remove")
+                Console.WriteLine($"{quantity} boxes of {boxType} removed from the farm.");
+            else
+                throw new ArgumentException($"Unknown operation type - {operationType}.");
+
+            PrintStorage();
+        }
+
+        public void PrintStorage()
+        {
+            Console.WriteLine("Current storage status: ");
+            Console.WriteLine($"Vegetable boxes: {_veggieBoxes}.");
+            Console.WriteLine($"Fruit boxes: {_fruitBoxes}.");
+        }
+    }
+}
---
+++ FarmApp/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace FarmApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var farm = new Farm();
+
+            Console.WriteLine("Avaliable box types: \n fruit \n vegetable \n");
+            Console.WriteLine("Enter a command (add [box type] [quantity] / remove [box type] [quantity] / exit): ");
+
+            string input;
+            
+            while ((input = Console.ReadLine().ToLower()) != "exit")
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    var result = ProcessCommand(input, farm);
+                    if (!result)
+                        Console.WriteLine("Invalid command, box type or quantity. Try again.");
+                } 
+                else
+                    Console.WriteLine("Input cannot be emty. Enter a valid command.");
+
+                Console.WriteLine("Enter a command (add [box type] [quantity] / remove [box type] [quantity] / exit): ");
+            }
+
+            Console.WriteLine("Exiting the program...");
+        }
+
+        static bool ProcessCommand(string input, Farm farm)
+        {
+            string[] command = input.Split(' ');
+
+            if (command.Length != 3)
+                return false;
+
+            if (!int.TryParse(command[2], out int quantity))
+                return false;
+
+            string operation = command[0];
+            string boxType = command[1];
+
+            if (boxType != "fruit" && boxType != "vegetable")
+                return false;
+
+            try
+            {
+                if (operation == "add")
+                    farm.AddBox(boxType, quantity);
+                else if (operation == "remove")
+                    farm.RemoveBox(boxType, quantity);
+                else
+                    return false;
+
+                return true;
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return true;
+        }
+    }
+}
