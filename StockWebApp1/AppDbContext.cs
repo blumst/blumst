a91298735e337c57e33ff1ff7a9f86b27f92624a
Mariia Khomych <masha.homich.696@gmail.com>
2024-07-08 00:38:21 +0300

    Created  StockWebApp

---
+++ StockWebApp1/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockWebApp1
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
---
+++ StockWebApp1/Program.cs
@@ -0,0 +1,37 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace StockWebApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StockWebApp1 API V1"));
+            }
+
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
